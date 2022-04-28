4f429684e46d7ede46309481606c6ce08b986852
Scott Bitterman <Scott.Bitterman@turner.com>
2022-04-28 05:53:19 -0400

    Clean up

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from './index';
 /*
     Start server and listen
+    
     Decoupling this compnent from the main route definitions 
     so that integration tests can run to test the routes 
     without also starting a server.
