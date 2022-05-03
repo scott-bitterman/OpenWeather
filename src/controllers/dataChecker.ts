35a4bfd4ac51758447526183bfcfea1ca7493565
Scott Bitterman <Scott.Bitterman@turner.com>
2022-05-03 03:26:59 -0400

    Comment

--- src/controllers/dataChecker.ts
+++ src/controllers/dataChecker.ts
@@ -1,3 +1,17 @@
 export function validLatituteAndLongitude(lat, lon) {
-    // TODO: check for valid lat and lon here
+    /* 
+        TODO: 
+        Check for valid lat and lon here!!!
+
+        Latitude and longitude are a pair of numbers (coordinates) 
+        used to describe a position on the plane 
+        of a geographic coordinate system. The numbers are 
+        in decimal degrees format and range from -90 to 90 for latitude 
+        and -180 to 180 for longitude.
+
+        Don't bother calling the OpenWeather API, if the values passed in 
+        are known to fail.
+    */
+
+
 }
